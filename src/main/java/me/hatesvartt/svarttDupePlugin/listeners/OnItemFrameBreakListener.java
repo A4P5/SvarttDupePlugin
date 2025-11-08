48d05f3b1ca51d853685cab0e0b5e669bca8d838
Svarttveil <85126010+A4P5@users.noreply.github.com>
2025-11-08 22:44:50 +0100

    Update OnItemFrameBreakListener.java

--- src/main/java/me/hatesvartt/svarttDupePlugin/listeners/OnItemFrameBreakListener.java
+++ src/main/java/me/hatesvartt/svarttDupePlugin/listeners/OnItemFrameBreakListener.java
@@ -1,4 +1,26 @@
+// OnItemFrameBreakListener.java
+
 package me.hatesvartt.svarttDupePlugin.listeners;
 
-public class OnItemFrameBreakListener {
+import io.papermc.paper.event.player.PlayerItemFrameChangeEvent;
+import io.papermc.paper.event.player.PlayerItemFrameChangeEvent.ItemFrameChangeAction;
+import me.hatesvartt.svarttDupePlugin.dupes.ItemFrameDupe;
+import me.hatesvartt.svarttDupePlugin.dupes.PistonDupe;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class OnItemFrameBreakListener implements Listener {
+
+    private final PistonDupe pistonDupe;
+    private final ItemFrameDupe itemFrameDupe;
+
+    public OnItemFrameBreakListener(ItemFrameDupe itemFrameDupe, PistonDupe pistonDupe) {
+        this.pistonDupe = pistonDupe; // use the same instance
+        this.itemFrameDupe = itemFrameDupe;
+    }
+
+    public void onItemFrameBreak(PlayerItemFrameChangeEvent event) {
+        pistonDupe.handleItemFrameBreak(event);
+        itemFrameDupe.handleItemFrameBreak(event);
+    }
 }
